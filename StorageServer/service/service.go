ae6accfebb280673d76ebab3daaaf265315e19bf
Rajiv Geraev <rajiv.geraev@tabby.ai>
2023-06-11 00:46:30 +0300

    T-0002: service layer - other

--- StorageServer/service/service.go
+++ StorageServer/service/service.go
@@ -12,6 +12,13 @@ import (
 	badger "github.com/dgraph-io/badger/v4"
 )
 
+type Service interface {
+	StoreChunk(id string, chunkNumber int, content io.Writer) error
+	DeleteChunks(id string) error
+	GetChunk(id string, chunkNumber int) (io.Reader, error)
+	GetChunkNumbers(id string) ([]int, error)
+}
+
 type BadgerStorageService struct {
 	db *badger.DB
 }
